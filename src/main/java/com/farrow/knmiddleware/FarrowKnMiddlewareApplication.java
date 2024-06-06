849e8c7ae178a0c389ae4944395c2693e695f5a3
Trung Nguyen <trung.nguyen@farrow.com>
2024-06-06 14:35:27 -0400

    Initial commit

---
+++ src/main/java/com/farrow/knmiddleware/FarrowKnMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.farrow.knmiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarrowKnMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarrowKnMiddlewareApplication.class, args);
+	}
+
+}
