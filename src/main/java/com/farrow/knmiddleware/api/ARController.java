fca7db9f05dfc4e16bc95b28400563fbb23fb6f3
fiacobaccifarrow <franco.iacobacci@farrow.com>
2024-06-19 10:04:41 -0400

    Added Queue schema

--- src/main/java/com/farrow/knmiddleware/api/ARController.java
+++ src/main/java/com/farrow/knmiddleware/api/ARController.java
@@ -1,10 +1,35 @@
 package com.farrow.knmiddleware.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/AR")
 public class ARController {
-	public void sendARInformation() {
-		
-		
-		
-		
+	private static final Logger log = LogManager.getLogger(ARController.class);
+	
+	@PostMapping("/as400")
+	public void receiveAs400ARPayLoad(HttpServletRequest req) throws IOException {
+		try(	InputStream is = req.getInputStream();
+				InputStreamReader ireader = new InputStreamReader(is);
+				BufferedReader reader = new BufferedReader(ireader);	){
+			
+			while(reader.ready()) {
+				String line = reader.readLine();
+				log.info(line);
+			}
+		}
 	}
+	
 }
