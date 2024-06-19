299a901309131d49d271bab2c3e67f0a3edcb97c
fiacobaccifarrow <franco.iacobacci@farrow.com>
2024-06-19 10:43:46 -0400

    Moved controller

--- src/main/java/com/farrow/knmiddleware/controllers/ARController.java
+++ src/main/java/com/farrow/knmiddleware/controllers/ARController.java
@@ -1,4 +1,4 @@
-package com.farrow.knmiddleware.api;
+package com.farrow.knmiddleware.controllers;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/AR")
@@ -20,7 +24,7 @@ public class ARController {
 	private static final Logger log = LogManager.getLogger(ARController.class);
 	
 	@PostMapping("/as400")
-	public void receiveAs400ARPayLoad(HttpServletRequest req) throws IOException {
+	public void receiveAs400ARPayLoad(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		try(	InputStream is = req.getInputStream();
 				InputStreamReader ireader = new InputStreamReader(is);
 				BufferedReader reader = new BufferedReader(ireader);	){
@@ -29,6 +33,12 @@ public class ARController {
 				String line = reader.readLine();
 				log.info(line);
 			}
+			
+		}
+		try(	OutputStream ros = resp.getOutputStream();
+				OutputStreamWriter rosw = new OutputStreamWriter(ros);
+				BufferedWriter writer = new BufferedWriter(rosw);){
+			writer.append("OUT234234   23423423 234 234 23 423 423 42");
 		}
 	}
 	
